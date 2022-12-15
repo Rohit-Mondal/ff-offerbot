573551703c70ca5e11fc690b43ddcafeed9b8f19
Rohit Mondal <rohitmondal003@gmail.com>
2022-12-15 18:17:22 +0530

    fixed sec

---
+++ middlewares/sectors1.js
@@ -0,0 +1,60 @@
+const sectors = [
+    {
+        dept: "BLR",
+        arrv: "CCU"
+    },
+    {
+        dept: "CCU",
+        arrv: "BLR"
+    },
+    {
+        dept: "BLR",
+        arrv: "DBR"
+    },
+    {
+        dept: "DBR",
+        arrv: "BLR"
+    },
+    {
+        dept: "BLR",
+        arrv: "DEL"
+    },
+    {
+        dept: "DEL",
+        arrv: "BLR"
+    },
+    {
+        dept: "CCU",
+        arrv: "HYD"
+    },
+    {
+        dept: "CCU",
+        arrv: "MAA"
+    },
+    {
+        dept: "MAA",
+        arrv: "CCU"
+    },
+    {
+        dept: "BLR",
+        arrv: "BOM"
+    },
+    {
+        dept: "BOM",
+        arrv: "BLR"
+    },
+    {
+        dept: "BLR",
+        arrv: "IXB"
+    },
+    {
+        dept: "IXB",
+        arrv: "BLR"
+    },
+    {
+        dept: "BLR",
+        arrv: "IXR"
+    },
+]
+
+module.exports = sectors
